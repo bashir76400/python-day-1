119552765b6341c364ed351358016289800ca17d
LUPIN <Lupin_254>
2024-09-16 19:24:23 +0300

    First Commit

---
+++ day.py
@@ -0,0 +1,12 @@
+age = 17
+name = "Bashir"
+language = "Python"
+
+age_input = input("Enter age: ")
+name_input = input("Enter name: ")
+language_input = input("Enter language: ")
+
+def nane(user):
+    print("My name is: " + user)
+
+nane("Bashir")
